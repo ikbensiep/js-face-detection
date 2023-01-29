c6bbbd2698ef47f2c681f7a304d16493cea02166
ikbensiep <ikbensiep>
2023-01-29 16:43:11 +0100

    make paths to models relative

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ const constraints = {
 const video = document.querySelector('video');
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models')
+  faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('./models')
 ]).then(startVideo)
 
 function startVideo() {
