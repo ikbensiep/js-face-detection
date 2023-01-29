ad6b4542f318cb9fb0deaade9d166c537b7b89ce
ikbensiep <ikbensiep>
2023-01-30 00:55:36 +0100

    Some gamification, needs fresh set of brains

--- script.js
+++ script.js
@@ -25,26 +25,104 @@ function startVideo() {
     console.error(`${err.name}: ${err.message}`);
   });
 
-  /*
-  navigator.getUserMedia(
-    { video: {} },
-    stream => video.srcObject = stream,
-    err => console.error(err)
-  )
-  */
+}
+const emotes = [
+  {name: 'neutral', emoji:'🙂', confidence:0},
+  {name: 'happy', emoji:'😃', confidence:0},
+  {name: 'sad', emoji:'😢', confidence:0},
+  {name: 'angry', emoji:'😠', confidence:0},
+  // {name: 'fearful', emoji:'😨', confidence:0},
+  {name: 'disgusted', emoji:'😩', confidence:0},
+  {name: 'surprised', emoji:'😯', confidence:0},
+];
+
+const facesDiv = document.querySelector('div');
+
+for (const emote of emotes) {
+  const span = document.createElement('span');
+  span.className = emote.name;
+  span.textContent = emote.emoji;
+  facesDiv.append(span);
+
+  const img = new Image();
+  img.className = emote.name;
+  document.querySelector('.gallery').append(img);
+}
+
+const generatePrompt = () => {
+  let item = emotes.find( item => item.confidence == 0);
+  let prompt = `${item.name}`;
+  
+  myprompt.textContent = prompt;
+  myprompt.dataset.emoji = (emotes.find( item => item.name == prompt).emoji);
+
+  return item;
+}
+
+let currentPrompt = generatePrompt();
+
+
+function cap(expression) {
+  let canvas = document.getElementById('canvas');
+  let video = document.getElementById('video');
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  canvas.getContext('2d').drawImage(video, 0, 0, video.videoWidth, video.videoHeight); // for drawing the video element on the canvas
+    
+  canvas.toBlob((blob) => { 
+    const img = new Image();
+    const url = URL.createObjectURL(blob);
+    img.onload = () => {
+      // no longer need to read the blob so it's revoked
+      URL.revokeObjectURL(url);
+    }
+    img.src = url;
+    const gallery = document.querySelector('.gallery');
+    const targetImage = gallery.querySelector(`img.${expression}`);
+    targetImage.src = img.src;
+    targetImage.scrollIntoView({behavior:"smooth"});
+    
+  });
 }
 
 video.addEventListener('play', () => {
-  const canvas = faceapi.createCanvasFromMedia(video)
-  document.body.append(canvas)
+  // const canvas1 = faceapi.createCanvasFromMedia(video)
+  // document.body.append(canvas1)
+  
   const displaySize = { width: video.width, height: video.height }
   faceapi.matchDimensions(canvas, displaySize)
-  setInterval(async () => {
-    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+  facesInterval = setInterval(async () => {
+    
+    if (!currentPrompt) {
+      document.querySelector('h1').style.opacity = 0;
+      video.style.opacity = 0;
+      clearInterval(facesInterval);
+    }
+
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions({ inputSize: 320 })).withFaceLandmarks().withFaceExpressions()
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
     //faceapi.draw.drawDetections(canvas, resizedDetections)
-    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-  }, 100)
-})
+    try {
+      const expressions = resizedDetections[0].expressions;
+      const keys = Object.keys(expressions);
+
+      keys.map( (key) => {
+        
+        if (currentPrompt && currentPrompt.confidence !==1 && expressions[currentPrompt.name] > 0.65) {
+          emotes.find( item => item.name == key).confidence = 1;
+          cap(currentPrompt.name);
+          document.querySelector(`.faces span.${currentPrompt.name}`).classList.add('success');
+          console.log("WELL DONE!", resizedDetections[0], resizedDetections[0].landmarks.getNose());
+          currentPrompt = generatePrompt();
+        }
+      });
+    } catch (e) {
+      console.log(currentPrompt)
+      console.error(e);
+      console.error(resizedDetections);
+      // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+      faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+    }
+  }, 1000).bind(this)
+});
